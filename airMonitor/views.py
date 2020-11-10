5d7b75d01ea03acde245360521f15661dab0b6f2
Habuon <danci99@protonmail.com>
2020-11-10 11:57:40 +0100

    Added ClassView

--- airMonitor/views.py
+++ airMonitor/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+import datetime
 
-# Create your views here.
+from django.shortcuts import render
+from django.views import View
+
+import json
+import random
+
+from airMonitor.models.Chart import Chart
+from airMonitor.models.SHMU import ObsNmsko1H
+from airMonitor.models.Station import Station
+
+
+class AirMonitorView(View):
+    def get(self, request):
+        data = Chart()
+
+        date_list = ["NO2", "PM10", "PM2,5", "O3"]
+        s = ObsNmsko1H.objects.all()
+
+        stations = Station.objects.all()
+
+        print(len(s))
+
+        for date in date_list:
+            for i in range(10):
+                data.add_data(str(date), random.random() * 5)
+                data.add_label(f"hour{i + 1}")
+
+        #print(json.dumps(data.dict(), indent=4))
+        return render(request, "chart.html", {'data': json.dumps(data.dict())})
+
+    def post(self, request):
+
+        return
