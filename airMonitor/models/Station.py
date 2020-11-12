d732000892ce4a2ffc75cb6425a9eb33dcc3c971
Habuon <danci99@protonmail.com>
2020-11-12 15:02:37 +0100

    Updated Station.py

--- airMonitor/models/Station.py
+++ airMonitor/models/Station.py
@@ -2,8 +2,9 @@ from airMonitor.models.SHMU import Si
 
 
 class Station(Si):
-    color = None
-    
+    color_name = None
+    color_code = None
+
     class Meta:
         managed = False
         db_table = 'si'
@@ -12,12 +13,3 @@ class Station(Si):
         self.color_name = color_name
         self.color_code = color_code
 
-    def get(self):
-        position = [self.lat, self.lon]
-        args = {
-            "color": self.color,
-            "fillColor": self.color,
-            "fillOpacity": 0.5,
-            "radius": 5000,
-        }
-        return {"position": position, "options": args}
