79d202894213ebb79e06da049433dc26b335f8e1
Habuon <danci99@protonmail.com>
2020-11-12 15:36:39 +0100

    Updated urls

--- vzduch/urls.py
+++ vzduch/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from airMonitor.views import AirMonitorView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', AirMonitorView.as_view()),
 ]
