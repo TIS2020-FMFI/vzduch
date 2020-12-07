9f4456d5ef2adac2fcab761d7c700b61ec0f7fbf
Habuon <danci99@protonmail.com>
2020-12-07 09:46:36 +0100

    Changed chart wrapper to work with days instead of hours

--- airMonitor/static/js/custom/ZL.js
+++ airMonitor/static/js/custom/ZL.js
@@ -6,16 +6,11 @@ class ZL{
         this.bg_color = bg_color;
     }
     get(stationName, number){
-        console.log(stationName);
         let d = this.data[stationName];
-        console.log(d);
         let n = number;
         if(n > d.length){
             n = d.length
         }
-        console.log(d.length);
-        console.log(n);
-        console.log(d.slice(d.length - n, d.length ));
         return {"label": this.name,
                 "fill": this.fill,
                 "backgroundColor": this.bg_color,
