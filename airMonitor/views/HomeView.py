882cada670a8eee1ee6072ef1b80ea7444cf8c33
Habuon <danci99@protonmail.com>
2020-12-10 16:40:45 +0100

    Fixed minor bug

--- airMonitor/views/HomeView.py
+++ airMonitor/views/HomeView.py
@@ -5,11 +5,10 @@ from django.shortcuts import render
 from django.views import View
 
 import json
-import random
 
 from airMonitor.forms.DateForm import DateForm
 from airMonitor.models.Chart import Chart
-from airMonitor.models.SHMU import ObsNmsko1H, Si
+from airMonitor.models.SHMU import ObsNmsko1H
 from airMonitor.models.Station import Station
 from airMonitor.models.AvgTable import AvgTable
 from airMonitor.models.StationsTable import StationsTable
@@ -41,7 +40,7 @@ class HomeView(View):
         zl = ObsNmsko1H.objects.all().filter(date__range=[date - datetime.timedelta(days=7),
                                                           date + datetime.timedelta(days=1)])
 
-        stations = add_colors(stations, zl.filter(date=date))
+        stations = add_colors(stations, zl.filter(date__range=[date, date + datetime.timedelta(days=1)]))
 
         for z in zl:
             for i in settings.ZL_LIMIT:
@@ -52,13 +51,10 @@ class HomeView(View):
         d = date + datetime.timedelta(days=1)
         d = datetime.datetime(d.year, d.month, d.day)
 
-        print(type(date))
-        print("date: ", d, d + datetime.timedelta(hours=5))
         for i in range(5):
             d = d + datetime.timedelta(hours=1)
             key = f"{d.day}.{d.month}.\n{str(d.hour).zfill(2)}:{str(d.minute).zfill(2)}"
             data.add_label(key)
-            print(key)
 
         return render(request, "final.html", {
             "data": json.dumps(data.dict()),
@@ -69,8 +65,5 @@ class HomeView(View):
 
     def post(self, request):
         global POST_DATA
-        # check whether it's valid:
-
         POST_DATA = request.POST
-
         return self.get(request)
