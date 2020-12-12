660925e0e4a97883d762f57a5b465f28df63daf6
Habuon <danci99@protonmail.com>
2020-12-12 11:39:24 +0100

    Removed stations and days column

--- airMonitor/forms/DateForm.py
+++ airMonitor/forms/DateForm.py
@@ -9,13 +9,10 @@ from airMonitor.models.Station import Station
 
 YEARS = set([x.year for x in ObsNmsko1H.objects.values_list('date', flat=True)])
 MONTHS = {x: M[x] for x in M if x in set([x.month for x in ObsNmsko1H.objects.values_list('date', flat=True)])}
-STATIONS = [(x.get_station().id, x.get_station().name) for x in Station.all()]
 
 
 class DateForm(Form):
-    date = forms.DateField(label='Datum', widget=forms.SelectDateWidget(years=YEARS, months=MONTHS), required=False)
-    days = forms.ChoiceField(choices=[(str(x), str(x)) for x in range(1, 8)], required=False)
-    stations = forms.ChoiceField(choices=STATIONS, required=False)
+    date = forms.DateField(label="", widget=forms.SelectDateWidget(years=YEARS, months=MONTHS), required=False)
 
     class Meta:
         pass
