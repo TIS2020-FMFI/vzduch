8726901d411bdf64db63c4aa190ed6e5672e0cbf
tomas.kunzo <tomas.kunzo@revolware.com>
2020-12-17 17:45:37 +0100

    display all dates in dateform

--- airMonitor/forms/DateForm.py
+++ airMonitor/forms/DateForm.py
@@ -7,12 +7,12 @@ from django.utils.dates import MONTHS as M
 from airMonitor.models.SHMU import ObsNmsko1H
 from airMonitor.models.Station import Station
 
-YEARS = set([x.year for x in ObsNmsko1H.objects.values_list('date', flat=True)])
-MONTHS = {x: M[x] for x in M if x in set([x.month for x in ObsNmsko1H.objects.values_list('date', flat=True)])}
+# YEARS = set([x.year for x in ObsNmsko1H.objects.values_list('date', flat=True)])
+# MONTHS = {x: M[x] for x in M if x in set([x.month for x in ObsNmsko1H.objects.values_list('date', flat=True)])}
 
 
 class DateForm(Form):
-    date = forms.DateField(label="", widget=forms.SelectDateWidget(years=YEARS, months=MONTHS), required=False)
+    date = forms.DateField(label="", widget=forms.SelectDateWidget(), required=False)
 
     class Meta:
         pass
