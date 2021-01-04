17fc28af1618636b21efee82740919f0f6b9a24d
Habuon <danci99@protonmail.com>
2021-01-04 18:48:50 +0100

    Fixed minor issue

--- airMonitor/forms/DateForm.py
+++ airMonitor/forms/DateForm.py
@@ -1,18 +1,13 @@
 from django import forms
-from django.forms import ModelForm, Form
-from django.utils.translation import gettext_lazy as _
-from django.utils.dates import MONTHS as M
-
+from django.forms import Form
 
 from airMonitor.models.SHMU import ObsNmsko1H
-from airMonitor.models.Station import Station
 
-# YEARS = set([x.year for x in ObsNmsko1H.objects.values_list('date', flat=True)])
-# MONTHS = {x: M[x] for x in M if x in set([x.month for x in ObsNmsko1H.objects.values_list('date', flat=True)])}
+YEARS = set(x.year for x in ObsNmsko1H.objects.values_list('date', flat=True))
 
 
 class DateForm(Form):
-    date = forms.DateField(label="", widget=forms.SelectDateWidget(), required=False)
+    date = forms.DateField(label="", widget=forms.SelectDateWidget(years=YEARS), required=False)
 
     class Meta:
         pass
