9d548f6e381ca75ff9a50a0cf2c4b7bdb418468d
Habuon <danci99@protonmail.com>
2021-01-07 12:51:38 +0100

    Refactored code

--- airMonitor/services/common.py
+++ airMonitor/services/common.py
@@ -6,13 +6,13 @@ def add_colors(stations, zl):
         mx = 0
         z = zl.filter(si=stations[i].get_station()).first()
         if z is not None:
-            pom = [[x, z.__dict__[x]] for x in settings.ZL_LIMIT]
+            pom = [[x, z.__dict__[x]] for x in settings.POLLUTANTS_LIMIT]
             for k in range(len(pom)):
                 for j in range(1, 5):
                     if pom[k][1] is None:
                         pom[k][1] = 0
                         break
-                    if pom[k][1] < settings.ZL_LIMIT[pom[k][0]][j]:
+                    if pom[k][1] < settings.POLLUTANTS_LIMIT[pom[k][0]][j]:
                         pom[k][1] = j
                         break
                     if j == 4:
