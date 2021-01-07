c0f421aab531afc2d722c02ce91e1455a3793daa
Habuon <danci99@protonmail.com>
2021-01-07 12:52:36 +0100

    Fixed logical flow

--- airMonitor/models/Chart.py
+++ airMonitor/models/Chart.py
@@ -6,47 +6,105 @@ from django.conf import settings
 class Chart:
     def __init__(self):
         self._chart = dict()
-        self._datasets = []
-        self.load_defaults("airMonitor/static/default_chart.json")
+        self._data = dict()
+        self._avg = dict()
+        self._datasets = list()
         self._labels = list()
+        self._stations = set()
+        self._pollutant = settings.POLLUTANTS
         self._data_colors = list(settings.DATA_COLORS.keys())
+
+        self.load_defaults("airMonitor/static/default_chart.json")
 
     def load_defaults(self, path):
         with open(path, "r") as file:
             self._chart = json.load(file)
-        self._chart["limits"] = settings.ZL_LIMIT
+        self._chart["limits"] = settings.POLLUTANTS_LIMIT
 
     def add_colors(self):
+        k = 0
         for index in range(len(self._datasets)):
-            self._datasets[index]["backgroundColor"] = settings.DATA_COLORS[self._data_colors[index]]
-            self._datasets[index]["borderColor"] = settings.DATA_COLORS[self._data_colors[index]]
+            pollutant = self._datasets[index]["label"]
+            print(pollutant)
+            if pollutant in settings.POLLUTANTS:
+                color_index = settings.POLLUTANTS.index(pollutant)
+            else:
+                color_index = len(settings.POLLUTANTS) + k
+                k += 1
+            print(color_index)
+            self._datasets[index]["backgroundColor"] = settings.DATA_COLORS[self._data_colors[color_index]]
+            self._datasets[index]["borderColor"] = settings.DATA_COLORS[self._data_colors[color_index]]
 
     def add_label(self, label):
         if label not in self._labels:
             self._labels.append(label)
 
+    def normalize_data(self):
+        self._generate_datasets()
+        self._generate_labels()
+
     def dict(self):
-        self.add_colors()
         self._chart["data"] = {
             "labels": [str(x) for x in self._labels],
             "datasets": self._datasets
         }
         return self._chart
 
-    def add_data(self, station, zl, data):
-        for dataset in self._datasets:
-            if dataset["label"] == zl:
-                if station not in dataset["data"]:
-                    dataset["data"][station] = []
-                dataset["data"][station].append(data)
-                return
-        self._datasets.append({
-            "label": zl,
-            "data": {station: [data, ]},
-            "fill": False
-        })
+    def add_data(self, date,  station, pollutant, data):
+        if pollutant == "avg":
+            if station not in self._avg:
+                self._avg[station] = []
+            self._avg[station].append(data)
+            return
+        self._stations.add(station)
+        if date not in self._data:
+            self._data[date] = dict()
+        if pollutant not in self._data[date]:
+            self._data[date][pollutant] = dict()
+        self._data[date][pollutant][station] = data
 
     def get_values(self, zl):
+        if len(self._datasets) == 0:
+            self._generate_datasets()
         for data in self._datasets:
             if data["label"] == zl:
                 return data
+
+    def _generate_datasets(self):
+        self._datasets = []
+        for pollutant in self._pollutant:
+            self._datasets.append({
+                "label": pollutant,
+                "data": dict(),
+                "fill": False
+            })
+        pollutant_data = dict()
+        for date in self._data:
+            for pollutant in self._pollutant:
+                if pollutant not in pollutant_data:
+                    pollutant_data[pollutant] = dict()
+                for station in self._stations:
+                    if station not in pollutant_data[pollutant]:
+                        pollutant_data[pollutant][station] = []
+                    if station in self._data[date][pollutant]:
+                        pollutant_data[pollutant][station].append(self._data[date][pollutant][station])
+                    else:
+                        pollutant_data[pollutant][station].append(None)
+
+        for i in range(len(self._datasets)):
+            self._datasets[i]["data"] = pollutant_data[self._datasets[i]["label"]]
+
+        self._datasets.append({
+            "label": "avg",
+            "data": self._avg,
+            "fill": False
+        })
+
+    def _generate_labels(self):
+        self._labels = []
+        for date in self._data:
+            if date is None:
+                continue
+            self._labels.append(date)
+
+
