ed34e38ca592ea91982ddba9ddc51632686421df
Habuon <danci99@protonmail.com>
2021-01-07 12:49:45 +0100

    Changed path to the one that is in settings

--- airMonitor/models/Wind.py
+++ airMonitor/models/Wind.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 import os
 
 
@@ -5,8 +6,8 @@ class Wind:
     def __init__(self):
         self.gust = []
         self.veind = []
-        self.gust_path = os.getcwd() + '/airMonitor/static/air/GUST'
-        self.veind_path = os.getcwd() + '/airMonitor/static/air/VEIND'
+        self.gust_path = settings.GUST_PATH
+        self.veind_path = settings.VEIND_PATH
 
     def load_data(self):
         self.gust = [file for file in os.listdir(self.gust_path)]
