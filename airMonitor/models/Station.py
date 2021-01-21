28c2cf4d2f32593bc8f641876c9b94379153c34b
DanielTrizna <54174205+DanielTrizna@users.noreply.github.com>
2021-01-21 12:19:06 +0100

    Added sql querry for getting stations

--- airMonitor/models/Station.py
+++ airMonitor/models/Station.py
@@ -1,21 +1,43 @@
 import json
 
-from airMonitor.models.SHMU import Si
+import pandas as pd
 
-from django.db.models import Q
+from airMonitor.services.database import Database
 
 
 class Station:
 
     @staticmethod
     def all():
-        result = []
-        for station in Si.objects.filter(ci=78).filter(~Q(lat=0.0)):
-            result.append(Station(station.name))
+        result = dict()
+        connection = Database.get_connection()
+        for station in pd.read_sql_query("SELECT * FROM si WHERE ci = 78", connection):
+            print(station)
+            #result.append(Station(station.name))
         return result
 
-    def __init__(self, name, color_name=None, color_code=None, zl=None):
-        self.station = Si.objects.all().filter(name=name).first()
+    class Si:
+        def __init__(self, data):
+            self.id = None
+            self.ci = None
+            self.ii = None
+            self.name = None
+            self.cccc = None
+            self.cc = None
+            self.iso_cc = None
+            self.lat = None
+            self.lon = None
+            self.elev = None
+            self.info = None
+            self.vtime = None
+            self.mtime = None
+            self.changed_id = None
+            self.changes = None
+            self.ue = None
+
+    def __init__(self, data, color_name=None, color_code=None, zl=None):
+
+        self.station = self.Si(data)
         self.color_name = color_name
         self.color_code = color_code
         self.zl = zl
