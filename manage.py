04338b89f030bc3fdf72334f346452ab844e1c00
DanielTrizna <54174205+DanielTrizna@users.noreply.github.com>
2021-01-21 12:20:50 +0100

    Added initialization of tunnel

--- manage.py
+++ manage.py
@@ -3,10 +3,14 @@
 import os
 import sys
 
+from airMonitor.services.tunnel import Tunnel
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vzduch.settings.base')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vzduch.settings.prod')
+
+    Tunnel.initialize("oko109", "ShmuHeslo1.")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
