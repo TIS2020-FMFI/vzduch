b8927212b3078f7a2aa8a387dc0b57e35b357c6e
DanielTrizna <54174205+DanielTrizna@users.noreply.github.com>
2021-01-26 10:02:05 +0100

    Removed tunnel initialization from start

--- vzduch/settings/prod.py
+++ vzduch/settings/prod.py
@@ -4,3 +4,7 @@ ALLOWED_HOSTS += ["10.20.7.241"]
 
 AIR_PATH = '/data/users/nwp122/ala2/png/sk10'
 
+TUNNEL_CREDENTIALS = {
+                    "username": "oko109",
+                    "password": "VlfasTP96A"
+                    }
