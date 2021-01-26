c0faf15261ea5cb097c2fce1bf64f5950abaee1f
DanielTrizna <54174205+DanielTrizna@users.noreply.github.com>
2021-01-26 09:53:59 +0100

    Added function for executing sql querries

--- airMonitor/services/database.py
+++ airMonitor/services/database.py
@@ -1,4 +1,9 @@
 import pymysql
+import pandas as pd
+from django.conf import settings
+from pymysql import DatabaseError
+
+from airMonitor.services.tunnel import Tunnel
 
 
 class Database:
@@ -8,3 +13,12 @@ class Database:
     def get_connection():
         return Database._connection
 
+    @staticmethod
+    def execute_sql(command):
+        username = settings.TUNNEL_CREDENTIALS["username"]
+        password = settings.TUNNEL_CREDENTIALS["password"]
+        for i in range(settings.RETRY_NUMBER):
+            try:
+                return pd.read_sql_query(command, Database._connection)
+            except DatabaseError:
+                Tunnel.initialize(username, password)
