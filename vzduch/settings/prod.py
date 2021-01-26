9a0ceee3f15ba585e5a323a20aa9a3eceba2fb4f
DanielTrizna <54174205+DanielTrizna@users.noreply.github.com>
2021-01-26 10:02:37 +0100

    Added tunnel credentials from file

--- vzduch/settings/prod.py
+++ vzduch/settings/prod.py
@@ -1,10 +1,8 @@
 from .base import *
+import  json
 
 ALLOWED_HOSTS += ["10.20.7.241"]
 
 AIR_PATH = '/data/users/nwp122/ala2/png/sk10'
 
-TUNNEL_CREDENTIALS = {
-                    "username": "oko109",
-                    "password": "VlfasTP96A"
-                    }
+TUNNEL_CREDENTIALS = json.load(open('private.json',))
