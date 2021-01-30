c938063780cf8f8e5ec456f8f3d9c0f4a7b18037
DanielTrizna <54174205+DanielTrizna@users.noreply.github.com>
2021-01-30 10:48:09 +0100

    Added function for changing pollutant and color of station

--- airMonitor/static/js/custom/Station.js
+++ airMonitor/static/js/custom/Station.js
@@ -1,18 +1,22 @@
 class Station{
-    constructor(name, lat, lon, color, color_code, zl) {
+    constructor(name, lat, lon) {
         this.name = name;
         this.lat = lat;
         this.lon = lon;
-        this.color = color;
-        this.color_code = color_code;
-        this.zl = zl;
+        this.color = null;
+        this.color_code = null;
+        this.pollutant = null;
+        this.value = null;
         this.radius = 5;
-        this.circle = L.circleMarker(this.getLocation(),{
-                color: this.color,
-                fillColor: this.color_code,
-                fillOpacity: 0.1,
-                radius: this.radius
-            });
+        this.circle = null
+        this.color_dict = {
+            0: ["grey", "#efefef"],
+            1: ["green", "#00b050"],
+            2: ["light green", "#92d050"],
+            3: ["yellow", "#ffff00"],
+            4: ["orange", "#ffc000"],
+            5: ["red", "#ff0000"]
+        }
     }
     getLocation(){
         return [this.lat, this.lon];
@@ -25,7 +29,7 @@ class Station{
                 fillOpacity: 1,
                 radius: this.radius
             });
-        this.circle.bindPopup(this.name + ': ' + this.zl);
+        this.circle.bindPopup(this.name + ': ' + this.pollutant + "( " + this.value + " )");
         this.circle.on('mouseover', function (e){
             this.openPopup();
         });
@@ -52,12 +56,14 @@ class Station{
     addTo(map){
         this.getCircle().addTo(map);
     }
-
-    increaseSize(){
-        this.radius *= 2;
+    changeColor(color){
+        this.color = color[0];
+        this.color_code = color[1];
     }
-
-    decreaseSize(){
-        this.radius /= 2;
+    changePollutant(pollutant, value){
+        this.pollutant = pollutant;
+        this.value = value;
+        let level = chart.getPollutantLevel(pollutant, value);
+        this.changeColor(this.color_dict[level]);
     }
 }
