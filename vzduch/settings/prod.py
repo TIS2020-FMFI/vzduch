d6627acac2086b5c5f065f36dc7496c0922a84de
DanielTrizna <54174205+DanielTrizna@users.noreply.github.com>
2021-02-01 17:29:45 +0100

    Removed loading of tunneling credentials

--- vzduch/settings/prod.py
+++ vzduch/settings/prod.py
@@ -5,4 +5,4 @@ ALLOWED_HOSTS += ["10.20.7.241"]
 
 AIR_PATH = '/data/users/nwp122/ala2/png/sk10'
 
-TUNNEL_CREDENTIALS = json.load(open('vzduch/settings/private.json',))
+# TUNNEL_CREDENTIALS = json.load(open('vzduch/settings/private.json',))
